c2a3a3387ac0047fc1847f68902add0eb98dec2a
Rajat Upadhyaya <rajat@urbanladder.com>
2014-05-18 08:07:32 +0000

    Add logging & save exception msg in failure

--- lib/melinis/task.rb
+++ lib/melinis/task.rb
@@ -1,6 +1,6 @@
 module Melinis
   class Task
-    attr_reader :last_run, :failures
+    attr_reader :last_run, :failures, :logger
 
     def initialize(task_name, options = {})
       options = {
@@ -13,8 +13,8 @@ module Melinis
       @task = Melinis::TaskList.find_or_initialize_by_name(task_name)
       @task.update_attributes(options)
 
+      @logger = Logger.new("log/%s.log" % [task_name.snakecase])
       @last_run = @task.task_processings.last
-      @current_run = Melinis::TaskProcessing.create!({:task_id => @task.id})
       @failures = @task.task_failures.to_be_processed(@task.individual_retries_limit)
     end
 
@@ -37,21 +37,25 @@ module Melinis
     def run
       success, total = 0, 0
       begin
+        @current_run = Melinis::TaskProcessing.create!({:task_id => @task.id})
+        logger.info { "Starting run #%d" % [@current_run.id] }
         data = prepare
+        total = data.size
         data.each do |unit|
-          total += 1
           begin
             execute(unit)
             success += 1
           rescue Exception => e
-            failure(execution_failure(unit))
+            failure(execution_failure(unit), {:exception => e})
+            logger.error { e }
           end
         end
       rescue Exception => e
-        failure({})
+        failure({}, {:exception => e})
+        logger.error { e }
       ensure
         @current_run.processed_details = wrapup.merge({
-          :success => success,
+          :success_count => success,
           :total => total
         }).to_yaml
         @current_run.save!
@@ -59,20 +63,20 @@ module Melinis
     end
 
     def failure(failure_details, args = {})
-      args = {:task_failure_id => nil, :status => 'failure'}.merge(args)
-      if args[:task_failure_id].nil?
+      task_failure_id = args[:task_failure_id]
+      data = failure_details.merge({:exception => args[:exception].to_s})
+      attrs = {
+        :failure_details => data.to_yaml,
+        :status => args[:status] || 'failure'
+      }
+      if task_failure_id.nil?
         Melinis::TaskFailure.create!({
           :task_processing_id => @current_run.id,
-          :task_id => @task.id,
-          :failure_details => failure_details.to_yaml,
-          :status => 'failure'
-        })
+          :task_id => @task.id
+        }.merge(attrs))
       else
-        task_failure = Melinis::TaskFailure.find_by_id(args[:task_failure_id])
-        task_failure.increment(:retry_count).update_attributes({
-          :failure_details => failure_details.to_yaml,
-          :status => args[:status]
-        })
+        task_failure = Melinis::TaskFailure.find_by_id(task_failure_id)
+        task_failure.increment(:retry_count).update_attributes(attrs)
       end
     end
   end
